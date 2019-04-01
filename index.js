7f083af359c768ba19e38e07175e3da73b45ff4b
Alvin Jackson <alvinjackson@Alvins-MBP.lan1>
2019-04-01 15:13:59 -0500

    added javascript

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+function getOutput(){
+	return document.getElementById("output-value").innerText;
+}
 
+function printOutput(num){
+	if(num==""){
+		document.getElementById("output-value").innerText=num;
+	}
+	else{
+		document.getElementById("output-value").innerText=getFormattedNumber(num);
+	}
+	document.getElementById("output-value").innerText=getFormattedNumber(num);
+}
+
+function getFormattedNumber(num){
+	var n = Number(num);
+	var value = n.toLocaleString("en");
+	return value;
+}
+
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''));
+}
+
+var operator = document.getElementsByClassName("operator");
+for (var i =0;i<operator.length;i++){
+	operator[i].addEventListener('click', function(){
+		alert("The operator clicked:"+this.id);
+	})
+}
+
+var number = document.getElementsByClassName("number");
+for (var i =0;i<number.length;i++){
+	number[i].addEventListener('click', function(){
+		var output=reverseNumberFormat(getOutput());
+		if(output !=NaN) {
+			output=output+this.id;
+			printOutput(output);
+		}
+	})
+}
