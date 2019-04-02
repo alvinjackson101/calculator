4b67d298d305c220f52d53a42fe4b67196e280b3
Alvin Jackson <alvinjackson@Alvins-MBP.attlocal.net>
2019-04-01 21:15:20 -0500

    updated javascript

--- index.js
+++ index.js
@@ -3,17 +3,18 @@ function getOutput(){
 }
 
 function printOutput(num){
-	if(num==""){
+	if(num === ""){
 		document.getElementById("output-value").innerText=num;
-	}
-	else{
+	} else {
 		document.getElementById("output-value").innerText=getFormattedNumber(num);
 	}
-	document.getElementById("output-value").innerText=getFormattedNumber(num);
+	// document.getElementById("output-value").innerText=getFormattedNumber(num);
 }
 
 function getFormattedNumber(num){
-	var n = Number(num);
+	// var n = Number(num);
+	let n = num
+	// console.log(n)
 	var value = n.toLocaleString("en");
 	return value;
 }
@@ -25,16 +26,20 @@ function reverseNumberFormat(num){
 var operator = document.getElementsByClassName("operator");
 for (var i =0;i<operator.length;i++){
 	operator[i].addEventListener('click', function(){
+
 		if(this.id=="ac"){
 			printOutput("");
 		}
 		else{
 			var output=getOutput();
+
 			if(output!=""){
-				output=reverseNumberFormat(output);
-				if(this.id=="="){
+				if(this.id==="btnequal"){
 					var result=eval(output);
+
 					printOutput(result);
+				} else {
+					printOutput(output+this.id)
 				}
 			}
 		}
@@ -44,7 +49,7 @@ for (var i =0;i<operator.length;i++){
 var number = document.getElementsByClassName("number");
 for (var i =0;i<number.length;i++){
 	number[i].addEventListener('click', function(){
-		var output=reverseNumberFormat(getOutput());
+		var output= getOutput()//reverseNumberFormat(getOutput());
 		if(output !=NaN) {
 			output=output+this.id;
 			printOutput(output);
